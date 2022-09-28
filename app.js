9b1f227085d94068ce4c052141e9f64b7d69f2ac
zioscuro <lore.rang@gmail.com>
2022-09-29 01:07:17 +0200

    Generate project gallery cards with javascript

---
+++ app.js
@@ -0,0 +1,38 @@
+const projects = [
+  'https://ifcjs.github.io/ifcjs-crash-course/sample-apps/01/',
+  'https://ifcjs.github.io/ifcjs-crash-course/sample-apps/02/',
+  'https://ifcjs.github.io/ifcjs-crash-course/sample-apps/03/',
+  'https://ifcjs.github.io/ifcjs-crash-course/sample-apps/04/',
+  'https://ifcjs.github.io/ifcjs-crash-course/sample-apps/05/',
+];
+
+const cardContainer = document.querySelector('.card-container');
+
+for (let project of projects) {
+  const card = document.createElement('article');
+  card.classList.add('card');
+
+  const icon = document.createElement('i');
+  icon.classList.add('fa-solid');
+  icon.classList.add('fa-building');
+
+  const projectTitle = document.createElement('h2');
+  projectTitle.classList.add('card-title');
+  projectTitle.textContent = `Project ${projects.indexOf(project) + 1}`;
+
+  const projectDescription = document.createElement('p');
+  projectDescription.classList.add('project-description');
+  projectDescription.textContent = 'this is a sample project for now...';
+
+  const viewButton = document.createElement('a');
+  viewButton.classList.add('card-link');
+  viewButton.href = './project.html';
+  viewButton.textContent = 'view';
+
+  card.appendChild(icon);
+  card.appendChild(projectTitle);
+  card.appendChild(projectDescription);
+  card.appendChild(viewButton);
+
+  cardContainer.appendChild(card);
+}
