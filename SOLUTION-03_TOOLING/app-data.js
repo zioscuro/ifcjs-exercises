a3bbd0b2d471e60de4e75bca93885b82f8ca1b7f
zioscuro <lore.rang@gmail.com>
2022-10-15 00:42:06 +0200

    Copy current files in tooling solution folder

---
+++ SOLUTION-03_TOOLING/app-data.js
@@ -0,0 +1,109 @@
+export const projects = [
+  {
+    id: 'project-001',
+    name: 'project 1',
+    author: 'zioscuro',
+    client: 'ifc.js crash course',
+    year: '2022',
+    type: 'conceptual design',
+    description: 'this is a sample project for now...',
+    link: 'https://ifcjs.github.io/ifcjs-crash-course/sample-apps/01/',
+    ifcPath: './IFC/01.ifc'
+  },
+  {
+    id: 'project-002',
+    name: 'project 2',
+    author: 'zioscuro',
+    client: 'ifc.js crash course',
+    year: '2022',
+    type: 'conceptual design',
+    description: 'this is a sample project for now...',
+    link: 'https://ifcjs.github.io/ifcjs-crash-course/sample-apps/02/',
+    ifcPath: './IFC/02.ifc'
+  },
+  {
+    id: 'project-003',
+    name: 'project 3',
+    author: 'zioscuro',
+    client: 'ifc.js crash course',
+    year: '2022',
+    type: 'conceptual design',
+    description: 'this is a sample project for now...',
+    link: 'https://ifcjs.github.io/ifcjs-crash-course/sample-apps/03/',
+    ifcPath: './IFC/03.ifc'
+  },
+  {
+    id: 'project-004',
+    name: 'project 4',
+    author: 'zioscuro',
+    client: 'ifc.js crash course',
+    year: '2022',
+    type: 'conceptual design',
+    description: 'this is a sample project for now...',
+    link: 'https://ifcjs.github.io/ifcjs-crash-course/sample-apps/04/',
+    ifcPath: './IFC/04.ifc'
+  },
+  {
+    id: 'project-005',
+    name: 'project 5',
+    author: 'zioscuro',
+    client: 'ifc.js crash course',
+    year: '2022',
+    type: 'conceptual design',
+    description: 'this is a sample project for now...',
+    link: 'https://ifcjs.github.io/ifcjs-crash-course/sample-apps/05/',
+    ifcPath: './IFC/05.ifc'
+  },
+  {
+    id: 'project-006',
+    name: 'project 6',
+    author: 'zioscuro',
+    client: 'ifc.js crash course',
+    year: '2022',
+    type: 'conceptual design',
+    description: 'this is a sample project for now...',
+    link: 'https://ifcjs.github.io/ifcjs-crash-course/sample-apps/05/',
+    ifcPath: './IFC/06.ifc'
+  },
+  {
+    id: 'project-007',
+    name: 'project 7',
+    author: 'zioscuro',
+    client: 'ifc.js crash course',
+    year: '2022',
+    type: 'conceptual design',
+    description: 'this is a sample project for now...',
+    link: 'https://ifcjs.github.io/ifcjs-crash-course/sample-apps/05/',
+    ifcPath: './IFC/07.ifc'
+  },
+  {
+    id: 'project-008',
+    name: 'project 8',
+    author: 'zioscuro',
+    client: 'ifc.js crash course',
+    year: '2022',
+    type: 'conceptual design',
+    description: 'this is a sample project for now...',
+    link: 'https://ifcjs.github.io/ifcjs-crash-course/sample-apps/05/',
+    ifcPath: './IFC/08.ifc'
+  },
+];
+
+export const controls = [
+  {
+    name: 'ruler',
+    icon: 'fa-ruler',
+  },
+  {
+    name: 'sectionBox',
+    icon: 'fa-cube',
+  },
+  {
+    name: 'hideElement',
+    icon: 'fa-glasses',
+  },
+  {
+    name: 'infoElement',
+    icon: 'fa-circle-info',
+  },
+];
